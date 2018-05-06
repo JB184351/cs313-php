8d76b62b8d9b3cdee7db389b166f22e5d81484b1
Justin Bengtson <justinbengtson@icloud.com>
2018-05-05 21:43:45 -0600

    Added php more code

--- web/homepage.php
+++ web/homepage.php
@@ -24,13 +24,9 @@
 
     <?php
 
-    $timezone = date_default_timezone_get();
+    date_default_timezone_set('America/Boise');
     echo "Current Date is " . date("Y/m/d") . "<br>";
-    echo "Current Time is " . $timezone;
-
-    echo date("l") . ", ";
-    echo date('F d') . " ";
-    echo date("h:i:sa");
+    echo "Current Time is " . date("h:i:sa");
 
     ?>
 
