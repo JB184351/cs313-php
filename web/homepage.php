c5bd278a77ea0a4875f462e6b04b88a551b5944a
Justin Bengtson <justinbengtson@icloud.com>
2018-05-05 19:47:54 -0600

    Finally fixed css issue I believe

--- web/homepage.php
+++ web/homepage.php
@@ -8,8 +8,8 @@
     <title>Justin Bengtson's HomePage</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="homepage.css">
-    <script src="hoempage.js"></script>
+    <link rel="stylesheet" type="text/css" href="week02/homepage.css">
+    <script src="week02/homepage.js"></script>
     
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
