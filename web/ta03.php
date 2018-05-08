1be65574762183f138d7c86dfa4610e21f55f8f2
Justin Bengtson <justinbengtson@icloud.com>
2018-05-08 16:36:39 -0600

    checkbox working?

--- web/ta03.php
+++ web/ta03.php
@@ -24,16 +24,16 @@
 
   Comments:<br>
 
-  <textarea name="Comments" rows="10" cols="30" placeholder="Comments"></textarea>
+  <textarea name="Comments" rows="10" cols="30" placeholder="Comments"></textarea><br>
 
   Places Visited:<br>
-  <input type="checkbox" name="continent" value="North America">North America<br>
-  <input type="checkbox" name="continent" value="South America">South America<br>
-  <input type="checkbox" name="continent" value="Europe">Europe<br>
-  <input type="checkbox" name="continent" value="Asia">Asia<br>
-  <input type="checkbox" name="continent" value="Austrailia">Austrailia<br>
-  <input type="checkbox" name="continent" value="Africa">Africa<br>
-  <input type="checkbox" name="continent" value="Antarctica">Antarctica<br>
+  <input type="checkbox" name="continent[]" value="North America">North America<br>
+  <input type="checkbox" name="continent[]" value="South America">South America<br>
+  <input type="checkbox" name="continent[]" value="Europe">Europe<br>
+  <input type="checkbox" name="continent[]" value="Asia">Asia<br>
+  <input type="checkbox" name="continent[]" value="Austrailia">Austrailia<br>
+  <input type="checkbox" name="continent[]" value="Africa">Africa<br>
+  <input type="checkbox" name="continent[]" value="Antarctica">Antarctica<br>
 
 
   <input type="submit" value="Submit">
@@ -49,17 +49,23 @@
 
 
   $Comments = $_POST["Comments"];
-  $Continent = $_POST["continent"];
+  $Continent = $_POST['continent'];
 
   echo $FirstName . "<br>";
   echo $LastName . "<br>";
   echo $Email . "<br>";
   echo $Major . "<br>";
   echo $Comments . "<br>";
-  echo $Continent . "<br>";
 
 
+  if(!empty($_POST['continent'])){
 
+    foreach($_POST['continent']){
+
+      echo $Continent . "<br>";
+    }
+
+  }
 
  ?>  
 
