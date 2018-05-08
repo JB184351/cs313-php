004d6b82046b74d9c7e73cd14e4c8e3c33e8a127
Justin Bengtson <justinbengtson@icloud.com>
2018-05-08 15:51:36 -0600

    Testing Form TA03

---
+++ ta03.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Team Activity 3</h2>
+
+<form action="/action_page.php">
+  First name:<br>
+  <input type="text" name="firstname" value="Mickey">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname" value="Mouse">
+  <br><br>
+  <input type="submit" value="Submit">
+
+  Email:<br>
+  <input type="text" value="">
+
+  Major:<br>
+  <input type="radio" name="gender" value="male" checked> Computer Science<br>
+  <input type="radio" name="gender" value="female"> Web Design and Development<br>
+  <input type="radio" name="gender" value="other"> CIT<br> 
+  <input type="radio" name="gender" value="other"> Computer Engineering<br>   
+
+</form> 
+
+</body>
+</html>
+
