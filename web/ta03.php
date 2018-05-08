e646f9da8def51c4972dd1eedcd3446be1715774
Justin Bengtson <justinbengtson@icloud.com>
2018-05-08 16:17:13 -0600

    Added more stuff

--- web/ta03.php
+++ web/ta03.php
@@ -6,21 +6,27 @@
 
 <form method="post">
   First name:<br>
-  <input type="text" name="firstname" value="">
+  <input type="text" name="firstname" placeholder="First Name">
   <br>
   Last name:<br>
-  <input type="text" name="lastname" value="">
+  <input type="text" name="lastname" placeholder="Last Name">
   <br><br>
-  <input type="submit" value="Submit">
+  
 
   Email:<br>
-  <input type="text" name="Email" value=""><br>
+  <input type="text" name="Email" placeholder="Email"><br>
 
   Major:<br>
-  <input type="radio" name="Major" value="male" checked> Computer Science<br>
-  <input type="radio" name="Major" value="female"> Web Design and Development<br>
-  <input type="radio" name="Major" value="other"> CIT<br> 
-  <input type="radio" name="Major" value="other"> Computer Engineering<br> 
+  <input type="radio" name="Major" value="Computer Science" checked> Computer Science<br>
+  <input type="radio" name="Major" value="Web Design and Devlopment"> Web Design and Development<br>
+  <input type="radio" name="Major" value="CIT"> CIT<br> 
+  <input type="radio" name="Major" value="Computer Engineering"> Computer Engineering<br> 
+
+  Comments:<br>
+
+  <textarea name="Comments" rows="10" cols="30" placeholder="Comments"></textarea>
+
+  <input type="submit" value="Submit">
 
  <?php
   
@@ -31,15 +37,20 @@
 
   $Major = $_POST["Major"];
 
+  $Comments = $_POST["Comments"];
+
   echo $FirstName . "<br>";
   echo $LastName . "<br>";
   echo $Email . "<br>";
   echo $Major . "<br>";
+  echo $Comments . "<br>";
 
 
  ?>  
 
 </form> 
+
+<br>s
 
 </body>
 </html>
