d19465a1371efa79589093e14613718d0f412d50
Justin Bengtson <justinbengtson@icloud.com>
2018-05-08 16:07:47 -0600

    Testing php again

--- web/ta03.php
+++ web/ta03.php
@@ -6,21 +6,37 @@
 
 <form action="/action_page.php">
   First name:<br>
-  <input type="text" name="firstname" value="Mickey">
+  <input type="text" name="firstname" value="">
   <br>
   Last name:<br>
-  <input type="text" name="lastname" value="Mouse">
+  <input type="text" name="lastname" value="">
   <br><br>
   <input type="submit" value="Submit">
 
   Email:<br>
-  <input type="text" value="">
+  <input type="text" name="Email" value=""><br>
 
   Major:<br>
-  <input type="radio" name="gender" value="male" checked> Computer Science<br>
-  <input type="radio" name="gender" value="female"> Web Design and Development<br>
-  <input type="radio" name="gender" value="other"> CIT<br> 
-  <input type="radio" name="gender" value="other"> Computer Engineering<br>   
+  <input type="radio" name="Major" value="male" checked> Computer Science<br>
+  <input type="radio" name="Major" value="female"> Web Design and Development<br>
+  <input type="radio" name="Major" value="other"> CIT<br> 
+  <input type="radio" name="Major" value="other"> Computer Engineering<br> 
+
+ <?php
+  $FirstName = _$POST["firstname"];
+  $LastName =  _$POST["lastname"];
+
+  $Email = _$POST["Email"];
+
+  $Major = _$POST["Major"];
+
+  echo $FirstName . "<br>";
+  echo $LastName . "<br>";
+  echo $Email . "<br>";
+  echo $Major . "<br>";
+
+
+ ?>  
 
 </form> 
 
