bee97a61dc666335884b0e20d853242560c45949
Justin Bengtson <justinbengtson@icloud.com>
2018-05-26 16:59:48 -0600

    Testing php 8

--- web/results.php
+++ web/results.php
@@ -9,23 +9,23 @@
 	$user_name = $_GET["person"];
 	echo "$user_name";
 
-$query = "SELECT score FROM score WHERE 
-person_id = (SELECT id FROM person WHERE name = '$user_name')";
-$statement = $db->prepare($query);
-$statement->bindValue(":rating", $user_name, PDO::PARAM_STR);
-$statement->execute();
+	$query = "SELECT score FROM score WHERE 
+	person_id = (SELECT id FROM person WHERE name = '$user_name')";
+	$statement = $db->prepare($query);
+	$statement->bindValue(":score", $user_name, PDO::PARAM_STR);
+	$statement->execute();
 
-foreach ($statement->fetchAll(PDO::FETCH_ASSOC) as $score)
-{
-    //$title = $movie["title"];
-   //$year = $movie["year"];
-   //$rating = $movie["code"];
+	foreach ($statement->fetchAll(PDO::FETCH_ASSOC) as $score)
+	{
+ 	   //$title = $movie["title"];
+ 	  //$year = $movie["year"];
+ 	  //$rating = $movie["code"];
 
-    $person = $score["person"];
-    $score1 = $score["score"];
+  	  $person = $score["person"];
+   	  $score1 = $score["score"];
     
-    echo "<li>$person - $score1</li>";
-}
+   	 echo "<li>$person - $score1</li>";
+	}
 ?>
 </body>
 </html>
