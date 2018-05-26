c4115ed3491c5e5ce66802b0b9fed6bccaa045f7
Justin Bengtson <justinbengtson@icloud.com>
2018-05-26 16:26:39 -0600

    Testing database access

--- web/results.php
+++ web/results.php
@@ -6,8 +6,25 @@
 <body>
 	<h1> Results!</h1>
 <?php
-	$user_score = $_GET["person"];
-	echo "$user_score";
+	$user_name = $_GET["person"];
+	echo "$user_name";
+
+$query = "SELECT * FROM score";
+$statement = $db->prepare($query);
+//$statement->bindValue(":rating", $user_rating, PDO::PARAM_STR);
+$statement->execute();
+
+foreach ($statement->fetchAll(PDO::FETCH_ASSOC) as $score)
+{
+    $title = $movie["title"];
+    $year = $movie["year"];
+    $rating = $movie["code"];
+
+    $person = $score["person"];
+    $score1 = $score["score"];
+    
+    echo "<li>$title ($year) - Rated $rating</li>";
+}
 ?>
 </body>
 </html>
