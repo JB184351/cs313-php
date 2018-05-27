5988856851a20b3fb01f2f301020421678cb9432
Justin Bengtson <justinbengtson@icloud.com>
2018-05-26 20:37:13 -0600

    Boostrap 4 Test

--- web/bowling.php
+++ web/bowling.php
@@ -5,7 +5,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
+	
+</head>
+<body>
+	<h1>Bowling</h1>
+
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bowling Score Tracker</title>
@@ -20,9 +25,7 @@
         crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-</head>
-<body>
-	<h1>Bowling</h1>
+
 
 <form action="results.php" method="get">
 
