54bdd807b2a2ba2057e255be58380bd623481d9b
Justin Bengtson <justinbengtson@icloud.com>
2018-05-31 15:36:01 -0600

    Testing things

---
+++ web/course.php
@@ -0,0 +1,19 @@
+<?php  
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Courses</title>
+	<h1>Courses</h1>
+</head>
+<body>
+	<ul>
+		<li><p>Course 1</p></li>
+		<li><p>Course 2</p></li>
+		<li><p>Course 3</p></li>
+		<li><p>Course 4</p></li>
+		<li><p>Course 5</p></li>
+
+</body>
+</html>
