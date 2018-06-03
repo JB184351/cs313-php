f3cde154ddfd2c982547ef01b1bc191388428337
Justin Bengtson <justinbengtson@icloud.com>
2018-06-02 20:53:50 -0600

    Testing getting name from php into datbase

--- web/Bowling/displayScore.php
+++ web/Bowling/displayScore.php
@@ -16,20 +16,20 @@ try{
 
 $personal_name = $_REQUEST["personal_name"];
 
-$sql = "INSERT INTO persons (personal_name) VALUES (:personal_name)";
+//$sql = "INSERT INTO persons (personal_name) VALUES (:personal_name)";
 
-$stmt->bindParam(':personal_name', $_REQUEST['personal_name']);
-$stmt->execute();
+//$stmt->bindParam(':personal_name', $_REQUEST['personal_name']);
+//$stmt->execute();
 echo "Inserted your score successfully!";
 }
 
-catch(PDOExeption $e){
-	die("ERROR: Data was not able to execute $sql. " . $e->getMessage());
-}
+//catch(PDOExeption $e){
+//	die("ERROR: Data was not able to execute $sql. " . $e->getMessage());
+//}
 
 echo "$personal_name";
 
-unset($pdo);
+//unset($pdo);
 
 ?>
 
