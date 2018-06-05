8d5011548038dec4052b678f3d237035bf84ae1e
Justin Bengtson <justinbengtson@icloud.com>
2018-06-05 16:20:00 -0600

    Testing Signup

---
+++ web/ta07/signup.php
@@ -0,0 +1,26 @@
+<?php
+
+include("dbConnect.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign up Page</title>
+	<h1>Sign up Here!!!!</h1>
+</head>
+<body>
+
+Sign Up Here!
+<form action="createUser" >
+
+<input type="text">Username: <br>
+<input type="text">Password: <br>
+<button type="submit">Sign Up!<br>
+
+ </form>
+
+</body>
+</html>
