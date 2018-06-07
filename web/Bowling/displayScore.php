3e90b8c7045998ddf44a7d4ce5daaa64bdced086
Justin Bengtson <justinbengtson@icloud.com>
2018-06-07 15:38:36 -0600

    Testing insert 12

--- web/Bowling/displayScore.php
+++ web/Bowling/displayScore.php
@@ -12,23 +12,21 @@ $personal_score = htmlspecialchars($_POST["personal_score"]);
 
 //$personal_data = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-//require("bowling.php");
-//require("dbConnect.php");
-//$db = get_db();
+require("bowling.php");
+require("dbConnect.php");
+$db = get_db();
 
-//$query = "INSERT INTO score (person_id, score) VALUES (':personal_name', :personal_score)";
+$query = "INSERT INTO score (person_id, score) VALUES ((SELECT id FROM person WHERE name =:personal_name), :personal_score)";
 
-//$statement = $db->prepare($query);
-//$statement->bindValue(":personal_name", $personal_name, PDO::PARAM_INT);
-//$statement->bindValue(":personal_score", $personal_score, PDO::PARAM_STR);
-//$statement->execute();
-    
-echo "<li>$personal_name - $personal_score</li>";
-	
+$statement = $db->prepare($query);
+$statement->bindValue(":personal_name", $personal_name, PDO::PARAM_INT);
+$statement->bindValue(":personal_score", $personal_score, PDO::PARAM_STR);
+$statement->execute();
+
+
+echo "<li>$personnal_name - $personal_score</li>";
 
 die();
-
-
 ?>
 
 </body>
