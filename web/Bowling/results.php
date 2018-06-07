89a3bc9933b283c18c2c212fb897133166a1042c
Justin Bengtson <justinbengtson@icloud.com>
2018-06-07 14:28:49 -0600

    Testing Inserting 8

--- web/Bowling/results.php
+++ web/Bowling/results.php
@@ -31,6 +31,28 @@
 <?php
 	require("dbConnect.php");
 
+    $user_name = $_GET["person"];
+
+    $query = "SELECT score.score, person.name AS person FROM score INNER JOIN person ON score.person_id = person.id WHERE person.name = :user_name";
+
+    // You would then prepare a statement based on that query:
+    $stmt = $db->prepare($query);
+    
+    // And then, you would bind a value to the prepared statement using
+    // 1) the name of the placeholder literal identifier from the SQL string
+    // 2) the variable that holds the value you want to inject into the SQL string
+    // 3) an optional PDO type hint
+    $stmt->bindValue('user_name', $user_name, PDO::PARAM_STR);
+    
+    // Finally, you would execute the prepared statement with bound values:
+    // (try/catch is included only to print errors, if something goes wrong)
+    try {
+        $stmt->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        die();
+    }
+
 	foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $score)
 	{
   	  $person = $score["person"];
