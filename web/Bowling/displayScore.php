6ad85cf31c8dc8f065b4bfc744e11386421ba47e
Justin Bengtson <justinbengtson@icloud.com>
2018-06-07 13:31:54 -0600

    Testing Inserting 2

--- web/Bowling/displayScore.php
+++ web/Bowling/displayScore.php
@@ -21,40 +21,13 @@ $statement = $db->prepare($query);
 $statement->bindValue(":personal_name", $personal_name, PDO::PARAM_INT);
 $statement->bindValue(":personal_score", $personal_score, PDO::PARAM_STR);
 $statement->execute();
-$personal_data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+echo "<li>$personnal_name - $personal_score</li>";
+
 die();
 
-/*foreach ($personal_data as $personal_data) {
-	$personal_name = $personal_data["personal_name"];
-	$personal_score = $personal_data["personal_score"];
-	echo "$personal_name<br>";
-	echo "$personal_score<br>";
-}
-*/
 
-//require("dbConnect.php");
-
-//try{
-
-//$person = $score["person"];
- //  	  $score1 = $score["score"];
-
-//$personal_name = $_REQUEST["personal_name"];
-
-//$sql = "INSERT INTO persons (personal_name) VALUES (:personal_name)";
-
-//$stmt->bindParam(':personal_name', $_REQUEST['personal_name']);
-//$stmt->execute();
-//echo "Inserted your score successfully!";
-//}
-
-//catch(PDOExeption $e){
-//	die("ERROR: Data was not able to execute $sql. " . $e->getMessage());
-//}
-
-//echo "$personal_name";
-
-//unset($pdo);
 ?>
 
 </body>
