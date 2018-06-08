dbf674745117f287364aaa2f7f9d4a1e67a3629f
Justin Bengtson <justinbengtson@icloud.com>
2018-06-07 20:46:58 -0600

    Testing insert 15

--- web/Bowling/displayScore.php
+++ web/Bowling/displayScore.php
@@ -10,19 +10,16 @@
 $personal_name = htmlspecialchars($_POST["personal_name"]);
 $personal_score = htmlspecialchars($_POST["personal_score"]);
 
-//$statement->fetchAll(PDO::FETCH_ASSOC);
-
-//require("bowling.php");
 require("dbConnect.php");
 $db = get_db();
 
-$query = "INSERT INTO score (person_id, score) VALUES ((SELECT id FROM person WHERE name = 'John', 124";
+$query = "INSERT INTO score (person_id, score) VALUES ((SELECT id FROM person WHERE name = :personal_name), :personal_score)";
 
 $statement = $db->prepare($query);
 $statement->bindValue(":personal_name", $personal_name, PDO::PARAM_INT);
 $statement->bindValue(":personal_score", $personal_score, PDO::PARAM_STR);
 $statement->execute();
-
+echo "$statement";
 
 echo "<li>$personnal_name - $personal_score</li>";
 
