600f71205688e8b3864fe5f67f357e804b0d91d1
Justin Bengtson <justinbengtson@icloud.com>
2018-06-08 08:36:54 -0600

    Testing insert 21

--- web/Bowling/createUser.php
+++ web/Bowling/createUser.php
@@ -12,33 +12,19 @@ $personal_score = htmlspecialchars($_POST["personal_score"]);
 
 require("dbConnect.php");
 
-if(isset($db))
-{
-	echo "Database is set";
-}
-
-else{
-	echo "Database is not set";
-}
-
 $query = "INSERT INTO person (name) VALUES (:personal_name)";
 
 $statement = $db->prepare($query);
 $statement->bindValue(":personal_name", $personal_name, PDO::PARAM_STR);
 $statement->execute();
 
-//$query = "INSERT INTO score (person_id, score) VALUES ((SELECT id FROM person WHERE name = :personal_name), :personal_score)";
+$query = "INSERT INTO score (person_id, score) VALUES ((SELECT id FROM person WHERE name = :personal_name), :personal_score)";
 
-
-/*()
 $statement = $db->prepare($query);
 $statement->bindValue(":personal_name", $personal_name, PDO::PARAM_STR);
 $statement->bindValue(":personal_score", $personal_score, PDO::PARAM_INT);
 $statement->execute();
-*/
 
-
-echo "$statement";
 
 echo "$personal_name - $personal_score";
 ?>
