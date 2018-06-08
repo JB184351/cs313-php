10a057f1d382a07aff967a915221eaf0bf53107c
Justin Bengtson <justinbengtson@icloud.com>
2018-06-07 20:44:09 -0600

    Testing insert 14

--- web/Bowling/displayScore.php
+++ web/Bowling/displayScore.php
@@ -16,7 +16,7 @@ $personal_score = htmlspecialchars($_POST["personal_score"]);
 require("dbConnect.php");
 $db = get_db();
 
-$query = "INSERT INTO score (person_id, score) VALUES ((SELECT id FROM person WHERE name =:personal_name, :personal_score)";
+$query = "INSERT INTO score (person_id, score) VALUES ((SELECT id FROM person WHERE name = 'John', 124";
 
 $statement = $db->prepare($query);
 $statement->bindValue(":personal_name", $personal_name, PDO::PARAM_INT);
