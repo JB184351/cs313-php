54daf7f3ebf4375abcfa36763172f82ae7656c8a
Justin Bengtson <justinbengtson@icloud.com>
2018-06-08 08:06:34 -0600

    Testing insert 17

--- web/Bowling/createUser.php
+++ web/Bowling/createUser.php
@@ -13,11 +13,11 @@ $personal_score = htmlspecialchars($_POST["personal_score"]);
 require("dbConnect.php");
 $db = get_db();
 
-$query = "INSERT INTO score (person_id, score) VALUES (SELECT id FROM person WHERE name = :personal_name, :personal_score)";
+$query = "INSERT INTO score (person_id, score) VALUES ((SELECT id FROM person WHERE name = :personal_name), :personal_score)";
 
 $statement = $db->prepare($query);
-$statement->bindValue(":personal_name", $personal_name, PDO::PARAM_INT);
-$statement->bindValue(":personal_score", $personal_score, PDO::PARAM_STR);
+$statement->bindValue(":personal_name", $personal_name, PDO::PARAM_STR);
+$statement->bindValue(":personal_score", $personal_score, PDO::PARAM_INT);
 $statement->execute();
 echo "$statement";
 
