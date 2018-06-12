5ddf24ce8d0c2a07390105c6ed63ca78589421bd
Justin Bengtson <justinbengtson@icloud.com>
2018-06-12 12:42:17 -0600

    Testing seeing all personal scores

--- web/Bowling/personal.php
+++ web/Bowling/personal.php
@@ -11,6 +11,27 @@
 
 	require("dbConnect.php");
 
+	$query = "SELECT score.score, person.name FROM score INNER JOIN person ON score.person_id = person.id";
+
+    $stmt = $db->prepare($query);
+    
+   
+    $stmt->bindValue('name', $query, PDO::PARAM_STR);
+
+    try {
+        $stmt->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        die();
+    }
+
+	foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $score)
+	{
+  	  $person = $score["person"];
+   	  $score1 = $score["score"];
+    
+   	 echo "<li style='color:white;'>" . $person . " - " . $score1 . "</li>";
+	}
 
   ?>
 
